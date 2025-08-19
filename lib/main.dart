dd3983f7cee7d32477858a63c451d416def5d05a
Noman Ali <ali.najfi009@gmail.com>
2025-08-19 13:52:23 +0500

    firebase fixes

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -5,6 +6,7 @@ import 'package:get_storage/get_storage.dart';
 import 'package:prime_social_media_flutter_ui_kit/config/app_color.dart';
 import 'package:prime_social_media_flutter_ui_kit/config/app_size.dart';
 import 'package:prime_social_media_flutter_ui_kit/config/app_string.dart';
+import 'package:prime_social_media_flutter_ui_kit/firebase_options.dart';
 import 'package:prime_social_media_flutter_ui_kit/services/db_service.dart';
 import 'package:prime_social_media_flutter_ui_kit/translation/app_translation.dart';
 import 'package:prime_social_media_flutter_ui_kit/views/splash/splash_view.dart';
@@ -16,6 +18,23 @@ import 'routes/app_routes.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await initTranslation();
+
+  try {
+    if (Firebase.apps.isEmpty) {
+      await Firebase.initializeApp(
+        options: DefaultFirebaseOptions.currentPlatform,
+      );
+    }
+  } catch (e) {
+    // Ignore duplicate initialization error
+    if (e
+        .toString()
+        .contains("A Firebase App named \"[DEFAULT]\" already exists")) {
+      // Already initialized, do nothing
+    } else {
+      rethrow; // Re-throw unexpected errors
+    }
+  }
 
   Get.put(
     DbService.init(GetStorage()),
