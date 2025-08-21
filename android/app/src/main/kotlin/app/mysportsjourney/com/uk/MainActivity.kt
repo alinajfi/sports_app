b56e74bb0b2fdfcf873086b2542b0fccdd659e76
Noman Ali <ali.najfi009@gmail.com>
2025-08-21 18:02:13 +0500

    some fixes and payment sheet setup

--- android/app/src/main/kotlin/app/mysportsjourney/com/uk/MainActivity.kt
+++ android/app/src/main/kotlin/app/mysportsjourney/com/uk/MainActivity.kt
@@ -1,6 +1,6 @@
 package app.mysportsjourney.com.uk
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity: FlutterActivity() {
+class MainActivity: FlutterFragmentActivity() {
 }
